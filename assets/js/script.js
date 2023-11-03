bc9397967025fe8a24b0d3e87d0f322eebddcab9
PengasKhan <cheesepuff42@gmail.com>
2023-11-02 20:47:36 -0500

    Scope fixed
    It was how I thought it would work when I imagined it originally! I was overthinking it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,20 +19,16 @@ function writePassword() {
       let myPassword = "";
       let chars = "";
       if (confirm("Include lowercase letters?")) {
-        let newString = chars.concat("abcdefghijklmnopqrstuvwxyz");
-        let chars = newString;
+        chars = chars.concat("abcdefghijklmnopqrstuvwxyz");
       }
       if (confirm("Include Uppercase letters?")) {
-        let newString = chars.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        let chars = newString;
+        chars = chars.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
       }
       if (confirm("Include Numbers?")) {
-        let newString = chars.concat("0123456789");
-        let chars = newString;
+        chars = chars.concat("0123456789");
       }
       if (confirm("Include Special Characters?")) {
-        let newString = chars.concat(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-        let chars = newString;
+        chars = chars.concat(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
       }
       for (let i = 0; i < len; i++)
         myPassword += chars.charAt(Math.floor(Math.random() * chars.length));
