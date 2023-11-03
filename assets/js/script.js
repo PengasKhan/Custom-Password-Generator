dc58c226982fe26cfcbacdf410f1becb53fbdc63
PengasKhan <cheesepuff42@gmail.com>
2023-11-02 22:21:19 -0500

    Working with comments
    After a few different roadblocks caused by overthinking a problem or second guessing myself, I have a function I feel pretty proud of! It's probably not the most efficient way possible to do this, but it feels pretty balanced in it's simplicity, length and self evident method. I'm sure I'll laugh at that claim once I actually start to know what I'm doing!

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,42 +7,50 @@ function writePassword() {
   const passwordText = document.querySelector("#password");
 
   function generatePassword() {
+    // prompts user for password length and converts it to an integer
     const lengthPrompt = parseInt(
       prompt("Enter a length for desired password (8-128)")
     );
+    // checks if length meets requirements
     if (lengthPrompt >= 8 && lengthPrompt <= 128) {
-      randomSet(lengthPrompt);
+      // continues process if requirements met
+      return randomSet(lengthPrompt);
+    // stops process if they aren't
     } else {
       return alert("You must enter a value between 8 and 128");
     }
+    // function to determine the character set desired and generating a random string using those characters
+    // our length we just determined is brought along for the end
     function randomSet(len) {
+      // starts with an empty string that is added to by .concat when the user affirms the character set to be used.
       let myPassword = "";
       let chars = "";
       if (confirm("Include lowercase letters?")) {
         chars = chars.concat("abcdefghijklmnopqrstuvwxyz");
-        console.log(chars);
       }
       if (confirm("Include Uppercase letters?")) {
         chars = chars.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        console.log(chars);
       }
       if (confirm("Include Numbers?")) {
         chars = chars.concat("0123456789");
-        console.log(chars);
       }
       if (confirm("Include Special Characters?")) {
         chars = chars.concat(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-        console.log(chars);
       }
+      // for loop of the length the user entered that picks a random character from the composite string we ended up with.
       for (let i = 0; i < len; i++)
         myPassword += chars.charAt(Math.floor(Math.random() * chars.length));
-      console.log(myPassword);
       return myPassword;
     }
-    return;
   }
-
-  passwordText.value = password;
+  // check that the password received from our functions isn't still the empty string or such
+  if (password) {
+    // displays the generated password if there is one
+    passwordText.value = password;
+  // kicks the user back to the start if they hit no to everything or something went wrong
+  } else {
+    return alert("You must choose at least one set of characters");
+  }
 }
 
 // Add event listener to generate button
